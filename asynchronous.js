f93f66f4f23fb90dafe409121dfbb401d1e3729c
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2020-12-18 14:02:45 +0700

    Asynchornous with Callback

--- asynchronous.js
+++ asynchronous.js
@@ -1,14 +1,40 @@
 
-////////////// ASYNCHRONOUS JAVASCRIPT
-const second=()=>{
+// ////////////// ASYNCHRONOUS JAVASCRIPT
+// const second=()=>{
+//     setTimeout(()=>{
+//         console.log(`Second`);
+//     },2000);
+// }
+// const first=()=>{
+//     console.log(`First`);
+//     second();
+//     console.log(`End`);
+// }
+// first();
+
+////////////////// ASYNCHRONOUS WITH CALLBACKS
+
+function getRecipe(){
     setTimeout(()=>{
-        console.log(`Second`);
-    },2000);
+        const recipeID=[523,883,432];
+        console.log(recipeID);
+
+        setTimeout((id)=>{
+            const recipe={title:'Fresh tomato pasta',
+                            publisher:'Minh'
+            }
+            console.log(`${id} : ${recipe.title}`);
+
+            setTimeout(publisher=>{
+                const recipe2={title:'Pizza',
+                            publisher:'Minh'
+            }
+            console.log(recipe2);
+            },1500,recipe.publisher)
+        },1000,recipeID[2]);
+
+    },1500)
 }
-const first=()=>{
-    console.log(`First`);
-    second();
-    console.log(`End`);
-}
-first();
+
+getRecipe();
 
