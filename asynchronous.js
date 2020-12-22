ac5474b47c027f6ba0c5d0f6d6ab1c48d1e8c4e3
Nguyen Thanh Minh <nguyenthanhminh112000@gmail.com>
2020-12-22 17:05:25 +0700

    Asynchronous with Async/Await

--- asynchronous.js
+++ asynchronous.js
@@ -40,8 +40,45 @@
 // }
 // getRecipe();
 
-/////////////////// ASYNCHRONOUS WITH PROMISES
+// /////////////////// ASYNCHRONOUS WITH PROMISES
 
+// const getIDs=new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve([523,883,432,974]);
+//     },0);
+// });
+// const getRecipe=function(recID){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout((id)=>{
+//             const recipe={title:'Fresh tomato pasta',publisher:'Minh'}
+//             resolve(recipe);
+//         },1500,recID)
+//     })
+// }
+// const getRelated=function(publisher){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout((publisher)=>{
+//             resolve(['Cookie',publisher]);
+//         },1500,publisher)
+//     })
+// }
+// getIDs
+// .then((IDs)=>{
+//     console.log()
+//     return getRecipe(IDs[0]);
+// })
+// .then(recipe=>{
+//     return getRelated(recipe.publisher);
+// })
+// .then(recipe2=>{
+//     console.log(recipe2);
+// })
+// .catch(error=>{
+//     console.log(`Error`);
+//     console.log(error);
+// })
+
+///////////////////// From Promises to AsyncAwait
 const getIDs=new Promise((resolve,reject)=>{
     setTimeout(()=>{
         resolve([523,883,432,974]);
@@ -52,30 +89,26 @@ const getRecipe=function(recID){
         setTimeout((id)=>{
             const recipe={title:'Fresh tomato pasta',publisher:'Minh'}
             resolve(recipe);
-        },1500,recID)
+        },0,recID)
     })
 }
 const getRelated=function(publisher){
     return new Promise((resolve,reject)=>{
         setTimeout((publisher)=>{
             resolve(['Cookie',publisher]);
-        },1500,publisher)
+        },0,publisher)
     })
 }
-getIDs
-.then((IDs)=>{
-    console.log()
-    return getRecipe(IDs[0]);
-})
-.then(recipe=>{
-    return getRelated(recipe.publisher);
-})
-.then(recipe2=>{
-    console.log(recipe2);
-})
-.catch(error=>{
-    console.log(`Error`);
-    console.log(error);
-})
 
-
+async function getRecipesAW(){
+    const IDs=await getIDs;
+    console.log(IDs);
+    const recipe=await getRecipe();
+    console.log(recipe);
+    const relateRecipe=await getRelated(recipe.publisher);
+    console.log(relateRecipe);
+}
+getRecipesAW();
+for(let i=0;i<5000;i++){
+    console.log(i);
+}
